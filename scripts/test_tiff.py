37182f7a4e7726a0eac7f21bb4a2d5020c316077
Filip Mazur <Filip.Mazur@pl.ey.com>
2025-03-09 16:18:50 +0100

    back to best v15

--- scripts/test_tiff.py
+++ scripts/test_tiff.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 
 # Open the TIFF file
-tiff_path = "./../S2_indices_v4_single_0601_0901.tiff"
+tiff_path = "./../S2_indices_v5_median_0601_0901.tiff"
 with rasterio.open(tiff_path) as src:
     # Print metadata
-    print("NDVI Tiff Bounds:", src.bounds)
+    print("Tiff Bounds:", src.bounds)
     print("CRS:", src.crs)
     print("Width, Height:", src.width, src.height)
     print("Number of bands:", src.count)
@@ -20,17 +20,17 @@ with rasterio.open(tiff_path) as src:
 # Plot the raster data
 plt.figure(figsize=(10, 6))
 plt.imshow(band1, cmap='RdYlGn', interpolation='nearest')
-plt.colorbar(label="NDVI Value")
-plt.title("NDVI TIFF Visualization")
+plt.colorbar(label="Value")
+plt.title("TIFF Visualization")
 plt.xlabel("Pixel Column")
-plt.ylabel("Pixel Row")pip
+plt.ylabel("Pixel Row")
 plt.show()
 
 # Plot a histogram of NDVI values
 plt.figure(figsize=(8, 5))
 plt.hist(band1.flatten(), bins=50, color='green', alpha=0.7)
-plt.title("Histogram of NDVI Values")
-plt.xlabel("NDVI Value")
+plt.title("Histogram of Values")
+plt.xlabel("Value")
 plt.ylabel("Frequency")
 plt.grid()
 plt.show()
